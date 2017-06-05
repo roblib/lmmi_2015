bb363d1a46997659dd05a7253a9057906de57546
rdrew <rdrew@vresupport.upei.ca>
2017-06-05 12:42:58 -0300

    .

--- template.php
+++ template.php
@@ -5,3 +5,11 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * lmmi_2015 theme.
  */
+function lmmi_2015_form_alter(&$form, &$form_state, $form_id) {
+
+	//if ($form_id == 'robs_testing_stuff_node_form') {
+	//dsm($form);
+
+	
+	//}
+}
